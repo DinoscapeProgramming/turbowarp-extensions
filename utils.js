cffc6b1f1dd1ca6960c96c664b9ee2f1a2ea8262
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2022-03-21 18:16:25 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,29 @@
+class Main {
+  getInfo () {
+    return {
+      id: "utils",
+      name: "Utils",
+      blocks: [{
+        opcode: 'isurl',
+        blocktype: Scratch.BlockType.REPORTER,
+        text: 'isURL [URL]',
+        arguments: {
+          URL: {
+            type: Scratch.ArgumentType.STRING,
+            defaultValue: 'URL'
+          }
+        }
+      }]
+    }
+  }
+  isurl (url) {
+    try {
+      new URL(url);
+    } catch {
+      return false;
+    }
+    return true;
+  }
+}
 
+Scratch.extensions.register(new Main())
